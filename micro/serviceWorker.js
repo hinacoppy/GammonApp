a433e51e8dbdab45eac50277c6f2ad3e5946ff9a
hinacoppy <kawakami@js4.so-net.ne.jp>
2024-06-11 18:12:47 +0900

    update serviceWorker.js and jquery ver-up

--- micro/serviceWorker.js
+++ micro/serviceWorker.js
@@ -2,7 +2,7 @@
 // (参考) https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Offline_Service_workers
 'use strict';
 
-const cacheName = 'MicroGammon-v20220308';
+const cacheName = 'MicroGammon-v20240611';
 const ORIGIN = (location.hostname == 'localhost') ? '' : location.protocol + '//' + location.hostname;
 
 const contentToCache = [
@@ -15,18 +15,18 @@ const contentToCache = [
   ORIGIN + '/GammonApp/micro/icon/android-chrome-192x192.png',
   ORIGIN + '/GammonApp/micro/icon/android-chrome-512x512.png',
   ORIGIN + '/GammonApp/css/bgboardapp.css',
+  ORIGIN + '/GammonApp/js/BgClockStub_class.js',
+  ORIGIN + '/GammonApp/js/BgKifuStub_class.js',
+  ORIGIN + '/GammonApp/js/BgGame_class.js',
   ORIGIN + '/css/bgAppBoard.css',
   ORIGIN + '/css/font-awesome-animation.min.css',
-  ORIGIN + '/js/fontawesome-all.min.js',
-  ORIGIN + '/js/jquery-3.6.0.min.js',
-  ORIGIN + '/js/inobounce.min.js',
   ORIGIN + '/js/BgAppBoard_class.js',
   ORIGIN + '/js/BgChequer_class.js',
   ORIGIN + '/js/BgXgid_class.js',
   ORIGIN + '/js/BgUtil_class.js',
-  ORIGIN + '/GammonApp/js/BgClockStub_class.js',
-  ORIGIN + '/GammonApp/js/BgKifuStub_class.js',
-  ORIGIN + '/GammonApp/js/BgGame_class.js'
+  ORIGIN + '/js/fontawesome-inuse.min.js',
+  ORIGIN + '/js/jquery-3.7.1.min.js',
+  ORIGIN + '/js/inobounce.min.js',
 ];
 
 self.addEventListener('install', (e) => {
@@ -35,13 +35,16 @@ self.addEventListener('install', (e) => {
       return cache.addAll(contentToCache);
     })
   );
+  self.skipWaiting();
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
-          cache.put(e.request, response.clone());
+          if (e.request.url.startsWith('http')) { //ignore chrome-extention: request (refuse error msg)
+            cache.put(e.request, response.clone());
+          }
           return response;
         });
       });
--- osampo/serviceWorker.js
+++ osampo/serviceWorker.js
@@ -2,7 +2,7 @@
 // (参考) https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Offline_Service_workers
 'use strict';
 
-const cacheName = 'Osampo-v20221209';
+const cacheName = 'Osampo-v20240611';
 const ORIGIN = (location.hostname == 'localhost') ? '' : location.protocol + '//' + location.hostname;
 
 const contentToCache = [
@@ -15,15 +15,15 @@ const contentToCache = [
   ORIGIN + '/GammonApp/osampo/icon/android-chrome-192x192.png',
   ORIGIN + '/GammonApp/osampo/icon/android-chrome-512x512.png',
   ORIGIN + '/GammonApp/osampo/css/OsampoGammon.css',
-  ORIGIN + '/css/font-awesome-animation.min.css',
-  ORIGIN + '/js/fontawesome-all.min.js',
-  ORIGIN + '/js/jquery-3.6.1.min.js',
-  ORIGIN + '/js/inobounce.min.js',
+  ORIGIN + '/GammonApp/osampo/js/BgBoard_class.js',
+  ORIGIN + '/GammonApp/osampo/js/BgGame_class.js'
   ORIGIN + '/js/BgChequer_class.js',
   ORIGIN + '/js/BgXgid_class.js',
   ORIGIN + '/js/BgUtil_class.js',
-  ORIGIN + '/GammonApp/osampo/js/BgBoard_class.js',
-  ORIGIN + '/GammonApp/osampo/js/BgGame_class.js'
+  ORIGIN + '/css/font-awesome-animation.min.css',
+  ORIGIN + '/js/fontawesome-inuse.min.js',
+  ORIGIN + '/js/jquery-3.7.1.min.js',
+  ORIGIN + '/js/inobounce.min.js',
 ];
 
 self.addEventListener('install', (e) => {
@@ -32,13 +32,16 @@ self.addEventListener('install', (e) => {
       return cache.addAll(contentToCache);
     })
   );
+  self.skipWaiting();
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
-          cache.put(e.request, response.clone());
+          if (e.request.url.startsWith('http')) { //ignore chrome-extention: request (refuse error msg)
+            cache.put(e.request, response.clone());
+          }
           return response;
         });
       });
